346d77df86fd7abda6ad6651835f01090d29b1d6
DanielVip3 <danieledemartino.72004@gmail.com>
2019-06-26 23:42:58 +0200

    Added presence and commented

--- bot.js
+++ bot.js
@@ -2,21 +2,38 @@ const constants = require('./constants.js');
 const client = constants.client;
 const token = constants.token;
 
+/* Imports all commands */
 require('./commands/commandsMain.js');
+/* Imports all events */
 require('./events/eventsMain.js');
 
+/* Announces when bot starts */
 client.on('ready', () => {
-  console.log(`
-  _    _                 __  __    __      __   _                 ____        _   
- | |  | |               |  \\/  |   \\ \\    / /  (_)               |  _ \\      | |  
- | |__| | ___  __ _ _ __| \\  / |_   \\ \\  / /__  _  ___ ___ ______| |_) | ___ | |_ 
- |  __  |/ _ \\/ _\` | '__| |\\/| | | | \\ \\/ / _ \\| |/ __/ _ \\______|  _ < / _ \\| __|
- | |  | |  __/ (_| | |  | |  | | |_| |\\  / (_) | | (_|  __/      | |_) | (_) | |_ 
- |_|  |_|\\___|\\__,_|_|  |_|  |_|\\__, | \\/ \\___/|_|\\___\\___|      |____/ \\___/ \\__|
-                                 __/ |                                            
-                                |___/                                                                                 
-										 `);
-  console.log("Bot started and operative. Have fun!")
+    console.log(`
+_    _                 __  __    __      __   _                 ____        _   
+| |  | |               |  \\/  |   \\ \\    / /  (_)               |  _ \\      | |  
+| |__| | ___  __ _ _ __| \\  / |_   \\ \\  / /__  _  ___ ___ ______| |_) | ___ | |_ 
+|  __  |/ _ \\/ _\` | '__| |\\/| | | | \\ \\/ / _ \\| |/ __/ _ \\______|  _ < / _ \\| __|
+| |  | |  __/ (_| | |  | |  | | |_| |\\  / (_) | | (_|  __/      | |_) | (_) | |_ 
+|_|  |_|\\___|\\__,_|_|  |_|  |_|\\__, | \\/ \\___/|_|\\___\\___|      |____/ \\___/ \\__|
+                             __/ |                                            
+                            |___/                                                                                 
+                                     `);
+    console.log("Bot started and operative. Have fun!");
+
+    /* Sets bot's presence */
+    client.user.setPresence({
+        activity: {
+            name: 'v!help',
+            type: 'STREAMING'
+        },
+        status: 'online',
+        timestamp: {
+            start: Date.now()
+        }
+    })
+
 });
 
+/* Log-ins with bot's credentials */
 client.login(token);
