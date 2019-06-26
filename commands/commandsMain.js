1e8d581656d151aedf161c0812b7f2a744222a69
DanielVip3 <danieledemartino.72004@gmail.com>
2019-06-26 23:40:54 +0200

    Added new stuck command

--- commands/commandsMain.js
+++ commands/commandsMain.js
@@ -1,4 +1,7 @@
+/* Main file that should group all commands */
+
 require('./help.js');
+require('./stuck.js');
 require('./recordVoice.js');
 require('./stopRecordingVoice.js');
 require('./voiceMessage.js');
---
+++ commands/stuck.js
@@ -0,0 +1,37 @@
+const constants = require('../constants.js');
+const prefix = constants.prefix;
+const client = constants.client;
+const audioStreamPerGuild = constants.audioStreamPerGuild;
+const voiceChannelPerGuild = constants.voiceChannelPerGuild;
+
+/* Command that should be used ONLY if the bot is stuck or bugged */
+client.on('message', async(message) => {
+    if (!message.author.bot && message.channel.type === "text" && message.content.startsWith(`${prefix}stuck`)) {
+        await message.react("✅");
+        message.channel.send("If you used this command, is because the bot is totally stuck or bugged. \nRemember that abusing this command will result in removing it from your server. \nEverything should be ok now.");
+
+        /* If bot is in a voice channel, leave it */
+        if (message.guild.me && message.guild.me.voice && message.guild.me.voice.channel) {
+            await message.guild.me.voice.channel.leave();
+        }
+
+        /* If guild is in audioStreamPerGuild object, remove it */
+        if (audioStreamPerGuild[message.guild.id]) {
+            audioStreamPerGuild[message.guild.id] = null;
+        }
+
+        /* If guild is in voiceChannelPerGuild object, first of all remove voice channel and its category, then remove the guild from the object */
+        if (voiceChannelPerGuild[message.guild.id]) {
+            /* voiceChannel created by the bot */
+            let voiceChannel = await message.guild.channels.find(channel => channel.id === voiceChannelPerGuild[message.guild.id]['voice_channel_id']);
+
+            if (voiceChannel) {
+                /* Deletes voice channel and its category */
+                await voiceChannel.parent.delete();
+                await voiceChannel.delete();
+            }
+
+            voiceChannelPerGuild[message.guild.id] = null;
+        }
+    }
+});
